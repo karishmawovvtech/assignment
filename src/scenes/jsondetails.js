60dc8221bb9c51459d5cd14b85709fae62d6d87d
eknath-kumbhar <eknathkumbharv1@hotmail.com>
2020-05-11 21:47:55 +0530

    keystore added for signed apk

--- src/scenes/jsondetails.js
+++ src/scenes/jsondetails.js
@@ -1,11 +1,14 @@
-import React,{Component} from 'react';
-import {View, Text} from 'react-native';
-export default class JSONDetails extends Component{
-    render(){
-        return(
-            <View>
-                <Text>hello</Text>
-            </View>
-        );
-    }
-}
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Container} from 'native-base';
+
+const JSONDetails = ({route}) => {
+  const {json} = route.params;
+  console.warn('route.params', route.params);
+  return (
+    <Container style={{padding: '5%'}}>
+      <Text>{JSON.stringify(json)}</Text>
+    </Container>
+  );
+};
+export default JSONDetails;
